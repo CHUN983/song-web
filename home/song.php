fb3c7c743c66db3a75b15021c0c48a79aca8dd75
puhua0210 <puhua0210@gmail.com>
2024-01-10 20:25:24 +0800

    song 修正

--- home/song.php
+++ home/song.php
@@ -1,57 +1,10 @@
-<head>
-    <?php
-        $list_id=$_GET["list_id"];
-        $list_name=$_GET["list_name"];
-    ?>
-    <meta charset="UTF-8">
-
-    $select_db = @mysql_select_db("local_band");
-    if (!$select_db) {
-        echo '<br>找不到資料庫!<br>';
-    } else {
-        $sql_query="SELECT * FROM user_playlist_song WHERE list_id='".$list_id."'";           
-        $result=mysql_query($sql_query);
-
-        if($result){
-            while($row=mysql_fetch_row($result)){
-                $sql_query1="SELECT * FROM song WHERE song_id='".$row[1]."'";      
-                $result1=mysql_query($sql_query1);
-                $info=mysql_fetch_row($result1);
-                
-                //$info[] 0為歌曲編號 1為歌曲所屬樂團 2為歌曲所屬專輯 3為歌曲名稱 4為歌曲長度
-            
-                echo '<br>歌名:'.$info[3];
-            
-                echo '  加入日期:'.$row[2].'<br>';
-            }
-        }else{
-            echo '<center> <table width=100% border=0 style="background-color: #e8e7d2;">'; // 設置背景色>';
-            echo '<tr>';
-            $i = 1;
-            $sum = 1;
-            while ($sum <= 25) {
-                
-                $sql_query = "select * from song where song_id='s_" . $i . "' ";
-                $result = mysql_query($sql_query);
-    
-                $row = mysql_fetch_row($result);
-                echo '<td width=20%><center><img src=音樂/'.$sum.'.jpg  width=100 height=100><br>';
-                echo '<a href=>' . $row[3] . '</a>';
-                $i += 4;
-                $sum++;
-                if($sum % 5 == 1) echo "<tr>";
-            }
-    
-            echo '</table>';
+<meta charset="UTF-8">
     <style>
         body {
             background-color: #e8e7d2; /* 設置背景顏色為 #e8e7d2 */
         }
         
     </style>
-</head>
-
-<body>
 
     <!-- ****************回首頁*************************** -->
     <form method="post" action="song.php">
@@ -62,11 +15,12 @@
     </form>
     <!-- ************************************************ -->
 
-
     <TT>
     <?php
         include("connection.php");
-
+        $list_id=$_GET["list_id"];
+        $list_name=$_GET["list_name"];
+  
         $select_db = @mysql_select_db("local_band");
         if (!$select_db) {
             echo '<br>找不到資料庫!<br>';
@@ -77,7 +31,7 @@
             echo '<body background-color: #e8e7d2>';
 
             //如果點擊列表出現列表，不然出現首頁
-            if(mysql_fetch_row($result)){
+            if(mysql_num_rows($result)){
                 
                 //引入script
                 echo '<script src="js/button.js"></script>';
@@ -116,6 +70,3 @@
 
         }
     ?>
-
-    <script src="function.js"></script>
-<body>
