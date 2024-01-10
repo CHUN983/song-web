bb02e87242d5f9c615da5d92adde835d1d92a241
sunthing <apple0908070605@gmail.com>
2024-01-10 21:21:39 +0800

    update

--- home/song.php
+++ home/song.php
@@ -7,7 +7,9 @@
     </style>
 
     <!-- ****************回首頁*************************** -->
-    <form method="post" action="song.php">
+    <form method="get" action="song.php">
+        <input type="hidden" id="back" name="back" value="1">
+
         <!-- 回首頁按鈕 -->
         <button type="submit">回首頁</button>
     </form>
@@ -18,33 +20,42 @@
         include("connection.php");
         $list_id=$_GET["list_id"];
         $list_name=$_GET["list_name"];
+
+        $back=$_GET["back"];
   
         $select_db = @mysql_select_db("local_band");
         if (!$select_db) {
             echo '<br>找不到資料庫!<br>';
-        } else {
-            $sql_query="SELECT * FROM user_playlist_song WHERE list_id='".$list_id."'";           
-            $result=mysql_query($sql_query);
+        } 
+        else {
             
             echo '<body background-color: #e8e7d2>';
 
             //如果點擊列表出現列表，不然出現首頁
-            if(mysql_num_rows($result)){
-                
+          
+            if($list_id){
+                $sql_query="SELECT * FROM user_playlist_song WHERE list_id='".$list_id."'";           
+                $result=mysql_query($sql_query);
+                if(mysql_num_rows($result)){
                 //引入script
-                echo '<script src="js/button.js"></script>';
-                while($row=mysql_fetch_row($result)){
-                    $sql_query1="SELECT * FROM song WHERE song_id='".$row[1]."'";      
-                    $result1=mysql_query($sql_query1);
-                    $info=mysql_fetch_row($result1);
+                    echo '<script src="js/button.js"></script>';
+                    while($row=mysql_fetch_row($result)){
+                        $sql_query1="SELECT * FROM song WHERE song_id='".$row[1]."'";      
+                        $result1=mysql_query($sql_query1);
+                        $info=mysql_fetch_row($result1);
+                        
+                        //$info[] 0為歌曲編號 1為歌曲所屬樂團 2為歌曲所屬專輯 3為歌曲名稱 4為歌曲長度
                     
-                    //$info[] 0為歌曲編號 1為歌曲所屬樂團 2為歌曲所屬專輯 3為歌曲名稱 4為歌曲長度
-                
-                    echo '<br>歌名:'.$info[3];
-                
-                    echo '  加入日期:'.$row[2].'<br>';
+                        echo '<br>歌名:'.$info[3];
+                    
+                        echo '  加入日期:'.$row[2].'<br>';
+                    }
                 }
-            }else{
+                else{
+                    echo '此歌單沒有歌曲!';
+                }
+            }
+            else{
                 echo '<center> <table width=100% border=0 style="background-color: #e8e7d2;">'; // 設置背景色>';
                 echo '<tr>';
                 $i = 1;
@@ -65,6 +76,8 @@
         
                 echo '</table>';
             }
-
         }
+            
+
+        
     ?>
